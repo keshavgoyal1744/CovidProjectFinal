59afe995bec4680455f1fccad9e46814b2546416
keshavgoyal1744 <keshavgoyal1744.kg@gmail.com>
2022-04-22 10:37:34 -0400

    Update CovidDataException.java

--- src/prj5/CovidDataException.java
+++ src/prj5/CovidDataException.java
@@ -7,7 +7,13 @@ package prj5;
  *
  */
 public class CovidDataException extends Exception {
-
+    /**
+     * This is the default constructor for the exception class
+     * 
+     * @param message
+     *            will be the message that will be displayed if the exception is
+     *            thrown.
+     */
     public CovidDataException(String message) {
 
         super(message);
